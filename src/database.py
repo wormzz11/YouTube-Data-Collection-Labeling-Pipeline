7b879dd2e24e838d848d4d3758231838bd991584
Krzysztof Łopiński <klopinski2@edu.cdv.pl>
2026-03-29 01:43:54 +0100

    working (probably) data injection (beta)

--- src/database.py
+++ src/database.py
@@ -1,25 +1,43 @@
 import sqlite3
 
-con = sqlite3.connect("data/database.db")
 
-cur = con.cursor()
+videos = [{'title': 'A bad day to use python', 'videoId': 'mx3g7XoPVNQ'}, {'title': '25 Tips &amp; Tricks in Python', 'videoId': 's_oXtdhqXR8'}, {'title': 'Python Full Course for Beginners', 'videoId': 'K5KVEU3aaeQ'}, {'title': 'you need to learn Python RIGHT NOW!! // EP 1', 'videoId': 'mRMmlo_Uqcs'}, {'title': 'Python in 100 Seconds', 'videoId': 'x7X9w_GIm1s'}]
 
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS yt_rel(
-        id INTEGER PRIMARY KEY,
-        videoId TEXT UNIQUE,
-        title TEXT,
-        relevant INTEGER
-    )
-""")
 
-cur.execute("""
-    INSERT into yt_rel(videoId, title, relevant) VALUES(
-        'test123', 
-        'machine learning',
-        1)     
-""")
-take = cur.execute("""SELECT * FROM yt_rel""")
-list = cur.fetchall()
-print(list)
-con.close()
+
+
+
+
+def db_creator():
+    con = sqlite3.connect("data/database.db")
+    cur = con.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS yt_rel(
+            id INTEGER PRIMARY KEY,
+            videoId TEXT UNIQUE,
+            title TEXT,
+            relevant INTEGER
+        )  
+    """)
+    con.close()
+
+
+
+
+
+def insert_videos(videos):
+    con = sqlite3.connect("data/database.db")
+    cur = con.cursor()
+    for video in videos:
+        videoId = video.get("videoId")
+        title = video.get("title")
+        relevant =  1
+
+        cur.execute("""
+            INSERT OR IGNORE INTO yt_rel(videoId, title, relevant) VALUES(
+                ?, ?,?
+                )     
+            """, (videoId,title, relevant))
+    con.commit()
+    con.close()
+
